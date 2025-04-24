37d7b2442406b3a37c6f72ab1127f61709e608cd
Oliver Storm <Oliverstorm@stofanet.dk>
2025-04-24 11:36:58 +0200

    Movement for the player and trying to get level editor in

--- platformer.py
+++ platformer.py
@@ -11,7 +11,7 @@ screen = pg.display.set_mode((screen_widht, screen_height))
 pg.display.set_caption("Platformer")
 
 # define game variables
-tile_size = 200
+tile_size = 50
 
 
 #Load images
@@ -20,10 +20,54 @@ bg_img = pg.image.load("img/sky.png")
 player_img = pg.image.load("img/sanke.png")
 
 
-def draw_grid():
-    for line in range(0,6):
-        pg.draw.line(screen, (255,255,255), (0,line * tile_size), (screen_widht, line * tile_size))
-        pg.draw.line(screen, (255,255,255), (line * tile_size, 0), (line * tile_size, screen_height))
+class Player():
+    def __init__(self,x,y):
+        img =pg.image.load("img/sanke.png")
+        self.img = pg.transform.scale(img, (80,80))
+        self.rect = self.img.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+        self.vel_y = 0
+        self.jumped = False
+
+    def update(self):
+
+        dx = 0
+        dy = 0
+        #get key presses
+        key = pg.key.get_pressed()
+        if key[pg.K_SPACE] and self.jumped == False:
+            self.vel_y = -15
+            self.jumped = True
+        if key[pg.K_SPACE] == False:
+            self.jumped = False
+        if key[pg.K_LEFT]:
+            dx -= 5
+        if key[pg.K_RIGHT]:
+            dx += 5
+
+        # add gravity
+        self.vel_y += 1
+        if self.vel_y > 10:
+            self.vel_y = 10
+        dy += self.vel_y
+
+        #Check for collision
+
+        #Update player position
+        self.rect.x += dx
+        self.rect.y += dy
+
+        if self.rect.bottom > screen_height:
+            self.rect.bottom = screen_height
+            dy = 0
+
+#Calculate new player posision
+#Check collision at new position
+#Adjust player position
+
+        #draw player onto screen
+        screen.blit(self.img, self.rect)
 
 class World():
     def __init__(self,data):
@@ -31,6 +75,7 @@ class World():
 
         # load images
         dirt_img = pg.image.load("img/dirt.png")
+        grass_img = pg.image.load("img/grass.png")
 
         # making variable for row, so it keeps track of the row
         row_count = 0
@@ -46,6 +91,14 @@ class World():
                     img_rect.y = row_count * tile_size
                     tile = (img, img_rect)
                     self.tile_list.append(tile)
+                if tile == 2:
+                    #draw dirt
+                    img = pg.transform.scale(grass_img, (tile_size, tile_size))
+                    img_rect = img.get_rect()
+                    img_rect.x = col_count * tile_size
+                    img_rect.y = row_count * tile_size
+                    tile = (img, img_rect)
+                    self.tile_list.append(tile)
                 col_count += 1
             row_count += 1
 
@@ -54,12 +107,25 @@ class World():
             screen.blit(tile[0], tile[1])
 
 world_data= [
-[1,1,1,1,1],
-[1,0,0,0,1],
-[1,0,0,0,1],
-[1,1,1,1,1],
+[1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
+[1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+[1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+[1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+[1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+[1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+[1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+[1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+[1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+[1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+[1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+[1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+[1,0,0,0,0,0,0,0,0,0,2,0,2,0,2,2,2,2,2,1],
+[1,0,0,0,0,2,2,2,2,0,0,0,0,0,1,1,1,1,1,1],
+[1,0,0,0,2,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
+[1,2,2,2,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
 ]
 
+player = Player(100, screen_height - 107)
 world = World(world_data)
 
 run = True
@@ -70,8 +136,8 @@ while run:
     screen.blit(sun_img, (100,100))
 
     world.draw()
+    player.update()
 
-    draw_grid()
 
     for event in pg.event.get():
         if event.type == pg.QUIT:
